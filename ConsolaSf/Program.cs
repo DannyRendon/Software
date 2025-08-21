eccb0cb26c9fd43fc694999e91d9e0ad71d36528
Danny RC <catanoandres131@gmail.com>
2025-08-21 09:21:59 -0500

    Videojuego New

--- ConsolaSf/Program.cs
+++ ConsolaSf/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Dispositivos
+
+{
+    public int Id;
+    public bool Valido;
+}
+
+
+public class Videojuegos
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Version;
+    public bool Existe;
+    public DateTime Fecha;
+    public Dispositivos? dispositivo; 
+
+}
